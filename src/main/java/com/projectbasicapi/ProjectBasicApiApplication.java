6d9473d3f74b2127d41b790d292e70ce0d3e082c
silviasalete <silviasalete@users.noreply.github.com>
2021-11-27 16:20:49 -0300

    :tada: Commit initial

---
+++ src/main/java/com/projectbasicapi/ProjectBasicApiApplication.java
@@ -0,0 +1,13 @@
+package com.projectbasicapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBasicApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectBasicApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projectbasicapi/ProjectBasicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectbasicapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBasicApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
